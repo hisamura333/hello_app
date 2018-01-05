eb885bf4a5a6647690afe62b63c359d701ecc7d1
Taiki Hisamura <taiki@fridaynight.co.jp>
2018-01-05 09:42:32 +0900

    modify static

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171228115804) do
+ActiveRecord::Schema.define(version: 20180102062803) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
