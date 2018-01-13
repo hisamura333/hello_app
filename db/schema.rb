76df4010654b6eedd7c5a71bd3f0c5ab230b2691
Taiki Hisamura <taiki@fridaynight.co.jp>
2018-01-13 16:28:35 +0900

    add secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110005726) do
+ActiveRecord::Schema.define(version: 20180113070800) do
 
   create_table "members", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "microposts", force: :cascade do |t|
--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
   def setup
-    @member = Member.new(name: "Example Member", email: "member@example.com")
+    @member = Member.new(name: "Example Member", email: "member@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -59,5 +59,15 @@ class MemberTest < ActiveSupport::TestCase
     @member.email = mixed_case_email
     @member.save
     assert_equal mixed_case_email.downcase, @member.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @member.password = @member.password_confirmation = " " * 6
+    assert_not @member.valid?
+  end
+
+  test "password should have a minimum length" do
+    @member.password = @member.password_confirmation = "a" * 5
+    assert_not @member.valid?
   end
 end
