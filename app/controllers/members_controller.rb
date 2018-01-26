601a1f16b9a5c1639d6a71146fb1901c8e14b25a
Taiki Hisamura <taiki@fridaynight.co.jp>
2018-01-26 22:09:06 +0900

    add strong paramater

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -6,4 +6,19 @@ class MembersController < ApplicationController
   def show
     @member = Member.find(params[:id])
   end
+
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :email, :password,:password_confirmation)
+  end
 end
