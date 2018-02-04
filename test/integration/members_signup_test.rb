0165868ae6e236102a26163421257d512218c597
hisamura333 <0aw2794w78t0b4c@ezweb.ne.jp>
2018-02-04 21:27:02 +0900

    add signup test

--- test/integration/members_signup_test.rb
+++ test/integration/members_signup_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class MembersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'Member.count' do
+      post members_path, params: { member: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'members/new'
+  end
 end
