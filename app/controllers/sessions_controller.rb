572d23379b62904fe7b80f321f272133f1240da5
Taiki Hisamura <taiki@fridaynight.co.jp>
2018-02-12 19:27:30 +0900

    make authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    member = Member.find_by(email: params[:session][:email].downcase)
+    if member && member.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      render 'new'
+    end
+  end
+
+  def destroy
   end
 end
