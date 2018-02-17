e6c84bdabe9de923d6eede277fde6578f1e6384e
hisamura333 <0aw2794w78t0b4c@ezweb.ne.jp>
2018-02-17 22:54:36 +0900

    add flash test

--- test/integration/members_login_test.rb
+++ test/integration/members_login_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class MembersLoginTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  get login_path
+  assert_template 'sessions/new'
+  post login_path, params: { session: { email: "", password: "" } }
+  assert_template 'sessions/new'
+  assert_not flash.empty?
+  get root_path
+  assert flash.empty?
 end
