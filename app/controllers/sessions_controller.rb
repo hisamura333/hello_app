726f4119d8bd4339ad8493b45d3bb7aed6edff51
Taiki Hisamura <taiki@fridaynight.co.jp>
2018-02-25 18:56:24 +0900

    create session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,4 @@
 class SessionsController < ApplicationController
-
   def new
-  end
-
-  def create
-    member = Member.find_by(email: params[:session][:email].downcase)
-    if member && member.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
-    else
-      # エラーメッセージを作成する
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
-    end
-  end
-
-  def destroy
   end
 end
