02f1bda95485305420b806a51726ddf66d214186
Taiki Hisamura <taiki@fridaynight.co.jp>
2018-03-18 16:48:18 +0900

    create session create api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    member = Member.find_by(email: params[:session][:email].downcase)
+    if member && member.authenticate(params[:session][:password])
+    else
+      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
 end
